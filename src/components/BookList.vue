33e139781bea35addb5a1715f452dfa1ba0456d8
lubenovt <tlubenov@vmware.com>
2019-11-24 11:48:40 +0200

    ex2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
 <div>
     <h1>{{title}}</h1>
+    <ol>
+        <li v-for="book in books" :key="book.title">
+            {{book.title}} {{book.author}}
+        </li>
+    </ol>
 </div>
 </template>
 
@@ -8,7 +13,12 @@
 export default {
     name: "BookList",
     data: {
-        title: "All Books"
+        title: "All Books",
+        books: [
+                {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                {title: 'American Gods', author: 'Neil Gaiman'},
+                {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                ]
     }
 }
 </script>
@@ -17,4 +27,13 @@ export default {
     h1, h2 {
         font-weight: normal;
     }
+    ol {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        display: block;
+        margin: 0 10px;
+    }
+
 </style>
