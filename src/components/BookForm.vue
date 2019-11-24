6cb068b49cf295c7ef85b379d4cfa7c0de54e192
lubenovt <tlubenov@vmware.com>
2019-11-24 12:21:28 +0200

    ex5 fix5

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,8 +1,8 @@
 <template>
-    <form class="" action="#" method="post">
+    <form v-on:submit.prevent="bookSubmit(bookAuthor, bookTitle)" class="" action="#" method="post">
         <input type="text" name="title" value="" placeholder="Book Title" v-model="bookTitle">
         <input type="text" name="author" value="" placeholder="Book Author" v-model="bookAuthor">
-        <button name="button" v-on:submit.prevent="bookSubmit(bookAuthor, bookTitle)">Add Book</button>
+        <button type="submit" name="button" >Add Book</button>
     </form>
 </template>
 
